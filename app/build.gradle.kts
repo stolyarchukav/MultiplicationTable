de3fcf7e8fe0389dc40fcb177ccac8083fa138f9
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2026-04-06 17:26:44 +0200

    release 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.stolyarchuk.multiplicationtable"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
